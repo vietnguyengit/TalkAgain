0830abfd36b15ee72b5c10772542f3b9781b44d1
vietnguyenswin <vietnguyenswin@outlook.com>
2017-08-30 19:16:03 +1000

    Initialised Upload

---
+++ clicks.js
@@ -0,0 +1,43 @@
+/**
+ * Created by vietnguyenswin on 8/30/17.
+ */
+
+function hungry() {
+    var audio = new Audio("sounds/hungry.mp3");
+    audio.play();
+}
+
+function thristy() {
+    var audio = new Audio("sounds/thirsty.mp3");
+    audio.play();
+}
+
+function sleepy() {
+    var audio = new Audio("sounds/sleepy.mp3");
+    audio.play();
+}
+
+function toilet() {
+    var audio = new Audio("sounds/toilet.mp3");
+    audio.play();
+}
+
+function headup() {
+    var audio = new Audio("sounds/headup.mp3");
+    audio.play();
+}
+
+function headdown() {
+    var audio = new Audio("sounds/headdown.mp3");
+    audio.play();
+}
+
+function pain() {
+    var audio = new Audio("sounds/pain.mp3");
+    audio.play();
+}
+
+function medic() {
+    var audio = new Audio("sounds/medic.mp3");
+    audio.play();
+}
